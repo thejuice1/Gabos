5d6c9bad5a78f345314556dc284973bb9c79e1c6
thejuice1 <84717070+thejuice1@users.noreply.github.com>
2021-05-24 16:22:34 +0200

    Add files via upload

---
+++ app.py
@@ -0,0 +1,21 @@
+#Lista
+gabos = ['Rafid', 'Shayan', 'Frans', 'Sina', 'Hassony', 'Hadde', 'Ali']
+
+#Definition
+Rafid = "Rafid is the richest in the group"
+Sina = "Sina is the poorest in the group"
+Shayan = "Shayan is 3rh richest in the group"
+Hassony = "Hassony is on Bill Gates level"
+Hadde = "Hadde is one of the richest in the group"
+Frans = "Frans, the famous arab Eagle is one of the richest in the group"
+Ali = "Ali is one of the richest in the group"
+
+#Kod
+print("Hi! Welcome to Forbes.")
+name = input("Please, enter your name: ")
+print("Hello " + name + "!")
+richest = input("Would you like to know the richest GABOS member? Answer with Yes/No: ")
+print("Here is the members list: ")
+print(gabos)
+name = input("Please, pick a name from the list to see rank: ")
+
